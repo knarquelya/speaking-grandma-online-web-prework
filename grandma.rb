c2af4a5d0b874a58a084ca9bbde25d45b4fa53da
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-02-05 16:26:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-speak_to_grandma = "I love you grandma!".upcase
+speak_to_grandma = "I love you grandma!" .upcase
 
 if speak_to_grandma == "I love you grandma!" .upcase
   puts "I love you too pumpkin!" .upcase
