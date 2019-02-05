a340c325a59b11a6a79275b7b551fa338c764e59
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-02-05 15:50:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 speak_to_grandma = "Hello Granny"
 
-if speak_to_grandma = "Hello Granny!"
+if speak_to_grandma == "Hello Granny!"
   puts "Huh?! Speak up, sonny!".upcase
-elsif speak_to_grandma = "I said hello Granny!"
+elsif speak_to_grandma == "I said hello Granny!"
   puts "No, not since 1938!".upcase
-else speak_to_grandma = "I love you grandma!".upcase
+else speak_to_grandma == "I love you grandma!".upcase
   puts "I love you too pumpkin!".upcase
 end
